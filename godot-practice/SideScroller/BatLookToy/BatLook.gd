cd5713128567aaec990020ea6103c90385068ad0
tomneo2004 <tomneo2004@gmail.com>
2022-10-14 11:49:45 +0800

    raycast

--- godot-practice/SideScroller/BatLookToy/BatLook.gd
+++ godot-practice/SideScroller/BatLookToy/BatLook.gd
@@ -7,3 +7,15 @@ var bat_textures := [
 ]
 
 onready var look_direction := $LookDirection
+onready var raycast := $RayCast2D
+
+func _physics_process(delta:float) -> void:
+	raycast.look_at(get_global_mouse_position())
+	raycast.force_raycast_update()
+	
+	if raycast.get_collider() is KinematicBody2D:
+		texture = bat_textures[0]
+	else:
+		texture = bat_textures[1]
+		
+	look_direction.vector = to_local(raycast.get_collision_point())
