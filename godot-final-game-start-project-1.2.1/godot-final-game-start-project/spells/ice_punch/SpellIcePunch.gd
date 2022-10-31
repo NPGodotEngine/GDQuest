29cfa7d5b41022ee4704360eaad7d8d8d825c09e
Nelson <tomneo2004@gmail.com>
2022-10-31 15:54:01 +0800

    spell ice punch

---
+++ godot-final-game-start-project-1.2.1/godot-final-game-start-project/spells/ice_punch/SpellIcePunch.gd
@@ -0,0 +1,10 @@
+class_name SpellIcePunch
+extends Spell
+
+func _physics_process(delta) -> void:
+	if Input.is_action_just_pressed("shoot") && _cooldown_timer.is_stopped():
+		shoot()
+
+func shoot() -> void:
+	_cooldown_timer.start()
+	.shoot()
