7d69a39d6d8bd111a28dc1310638bf1e152fdce9
Nelson <tomneo2004@gmail.com>
2022-11-01 10:00:27 +0800

    add shots per fire for spell ice punch

--- godot-final-game-start-project-1.2.1/godot-final-game-start-project/spells/ice_punch/SpellIcePunch.gd
+++ godot-final-game-start-project-1.2.1/godot-final-game-start-project/spells/ice_punch/SpellIcePunch.gd
@@ -1,10 +1,21 @@
 class_name SpellIcePunch
 extends Spell
 
+export var num_shots_per_fire := 3
+
 func _physics_process(delta) -> void:
 	if Input.is_action_just_pressed("shoot") && _cooldown_timer.is_stopped():
 		shoot()
 		 
 func shoot() -> void:
 	_cooldown_timer.start()
-	.shoot()
+	
+	for _i in num_shots_per_fire:
+		var bullet: Bullet = bullet_scene.instance()
+		get_tree().root.add_child(bullet)
+		bullet.global_transform = global_transform
+		bullet.randomize_rotation(deg2rad(random_angle_degrees))
+		bullet.speed = max_bullet_speed
+		bullet.max_range = max_range
+		
+	_audio.play()
